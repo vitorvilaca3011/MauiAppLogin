c37d407bd56b9db46e99177ae3896d79f6feda46
Vitor Gabriel da Silva <vitorvilaca3011@gmail.com>
2025-04-16 19:46:27 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace MauiAppLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            string? usuario_logado = null;
+
+            // Enviando o usuário ara página de login por padrão.
+            MainPage = new Login();
+
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+
+                if (usuario_logado != null)
+                {
+                    MainPage = new Protegida();
+                }
+
+                /*if(usuario_logado == null)
+                {
+                    MainPage = new Login();
+
+                } else
+                {
+                    MainPage = new Protegida();
+                }*/
+            });
+        }
+        
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+
+    } // Fecha classe
+} // Fecha namespace
